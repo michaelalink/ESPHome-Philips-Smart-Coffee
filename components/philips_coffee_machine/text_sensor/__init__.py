bf5ebc555863420aea0de08742facd35a165b900
michaelalink <michaelalink@gmail.com>
2026-01-10 20:14:30 -0600

    Update __init__.py

--- components/philips_coffee_machine/text_sensor/__init__.py
+++ components/philips_coffee_machine/text_sensor/__init__.py
@@ -13,7 +13,8 @@ StatusSensor = philips_status_sensor_ns.class_(
 )
 
 CONFIG_SCHEMA = (
-    text_sensor._TEXT_SENSOR_SCHEMA.extend(
+    text_sensor._TEXT_SENSOR_SCHEMA
+    .extend(
         {
             cv.GenerateID(): cv.declare_id(StatusSensor),
             cv.Required(CONTROLLER_ID): cv.use_id(PhilipsCoffeeMachine),
